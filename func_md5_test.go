3b46dd2ba28b7c3f8e929375ed416e9ca2ed02ac
Steve High <steve.high@gmail.com>
2017-04-04 22:44:12 -0400

    added sha256 hash function

--- func_md5_test.go
+++ func_md5_test.go
@@ -1,6 +1,7 @@
 package str
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +9,9 @@ import (
 
 func TestMD5(t *testing.T) {
 	assert.Equal(t, "1d340856fb75496176c4b968925452e7", MD5("This is a string of tests. 世界"))
+}
+
+func ExampleMD5() {
+	fmt.Println(MD5("This is a string of tests. 世界"))
+	// Output: 1d340856fb75496176c4b968925452e7
 }
