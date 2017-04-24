aed6ed62589e8876ba0097555f76f3ff6e5d0156
Steve High <steve.high@gmail.com>
2017-04-24 00:01:14 -0400

    token stuff

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 [![Build Status](https://travis-ci.org/schigh/str.svg?branch=master)](https://travis-ci.org/schigh/str)
 [![GoDoc](https://godoc.org/github.com/schigh/str?status.svg)](https://godoc.org/github.com/schigh/str)
 
+
 # str
 The missing Golang Strings Library
 
---
+++ token.go
@@ -0,0 +1,52 @@
+package str
+
+import (
+	"math/rand"
+	"time"
+)
+
+type TokenOptions struct {
+	Length  uint
+	Charset string
+	Prefix  string
+}
+
+var defaultTokenOptions *TokenOptions
+
+func init() {
+	defaultTokenOptions = &TokenOptions{
+		Length:  20,
+		Charset: "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789",
+	}
+	rand.Seed(time.Now().UnixNano())
+}
+
+// SetTokenOptions - set the options for token generation
+func SetTokenOptions(options *TokenOptions) {
+	defaultTokenOptions = options
+}
+
+// Token - create pseudorandom token
+func Token() string {
+	return TokenWithOptions(defaultTokenOptions)
+}
+
+// TokenWithOptions - create pseudorandom token with user-supplied options
+func TokenWithOptions(options *TokenOptions) string {
+	cl := len(options.Charset)
+	pl := len(options.Prefix)
+	if options.Length < 1 || cl == 0 {
+		return ""
+	}
+	if pl >= int(options.Length) {
+		return options.Prefix
+	}
+	rounds := int(options.Length) - pl
+	token := ""
+	for i := 0; i < rounds; i++ {
+		guard := rand.Intn(cl)
+		token += options.Charset[guard : guard+1]
+	}
+
+	return options.Prefix + token
+}
